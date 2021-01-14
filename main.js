42ba2680d631d93e8dfaef969fa2fed0172384b0
Aline Benvenuto <aline.benvenutto@gmail.com>
2021-01-14 17:08:07 -0300

    Conhecendo as funcionalidades básicas JS

---
+++ main.js
@@ -0,0 +1,71 @@
+/*var nome = "Aline Benvenuto"
+var idade = 26
+var idade2 = 22
+var frase = "Japão é o melhor time do mundo"
+alert(nome + " tem " + idade + " anos");
+alert(idade + idade2)
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão","Brasil")); 
+console.log(frase.toUpperCase ());
+console.log(frase.toLowerCase());
+alert(frase.replace("Japão", "Brasil"));
+*/
+
+//LISTAS = ARRAY
+
+/*var list = ["maça","pera","laranja"];
+list.push ("uva");
+list.pop ("uva");
+console.log(list);
+console.log(list.length); //tamanho da lista
+console.log(list.reverse()); // inverte a ordem
+console.log(list.join( " - "));
+console.log(list[1]);
+alert(list[1]);
+
+*/
+
+//DICIONÁRIO
+
+/*var fruta = {nome:"maça", cor:"red"}
+console.log(fruta);
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+// LISTA + DICIONÁRIO
+
+/*var frutas = [{nome:"maça", cor:"red"}, {nome:"uva", cor:"purple"}]
+console.log(frutas);
+alert(frutas[0].nome);
+*/
+
+//CONDICIONAIS + LAÇO DE REPETIÇÃO
+
+/*var idade = prompt ("Qual sua idade?");
+//var idade = 16;
+if (idade >=18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+*/
+
+/*
+var count = 0;
+while (count <5){
+    alert(count);
+    count++;
+}
+*/
+
+/*
+var count;
+for (count=0; count<= 5; count++){
+    alert(count);
+}
+*/
+
+
+
